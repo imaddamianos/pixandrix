66bd94200b3a331632b4015b8823c5327e6b29f2
iMad <damianos.imad@gmail.com>
2024-04-16 01:53:43 +0300

    first init

---
+++ android/app/src/main/kotlin/com/example/pixandrix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pixandrix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/first_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pix and Rix'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              SizedBox(
+              width: 120,
+              height: 120,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle driver button press
+                  // Navigate to driver login/register page
+                },
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/driver_icon.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Drivers'),
+                  ],
+                ),
+              ),
+            ),
+                const SizedBox(width: 20),
+               SizedBox(
+              width: 120,
+              height: 120,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle driver button press
+                  // Navigate to driver login/register page
+                },
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/stores_icon.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Store'),
+                  ],
+                ),
+              ),
+            ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Handle admin panel button press
+                // Navigate to admin panel login page
+              },
+              child: const Text(
+                'Admin Panel',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
