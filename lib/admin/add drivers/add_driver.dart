4295d0ef36a99426d3db39727647677ee84e4ead
iMad <damianos.imad@gmail.com>
2024-04-18 11:05:24 +0300

    adding add drivers

--- lib/admin/add drivers/add_driver.dart
+++ lib/admin/add drivers/add_driver.dart
@@ -2,32 +2,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:pixandrix/admin/admin_panel.dart';
 
-class AddOwnerPage extends StatefulWidget {
-  const AddOwnerPage({super.key});
+class AddDriverPage extends StatefulWidget {
+  const AddDriverPage({super.key});
 
   @override
   _AddOwnerPageState createState() => _AddOwnerPageState();
 }
 
-class _AddOwnerPageState extends State<AddOwnerPage> {
+class _AddOwnerPageState extends State<AddDriverPage> {
   final _formKey = GlobalKey<FormState>();
   late TextEditingController _nameController;
   late TextEditingController _phoneNumberController;
-  late TextEditingController _locationController;
+  // late TextEditingController _locationController;
 
   @override
   void initState() {
     super.initState();
     _nameController = TextEditingController();
     _phoneNumberController = TextEditingController();
-    _locationController = TextEditingController();
+    // _locationController = TextEditingController();
   }
 
   @override
   void dispose() {
     _nameController.dispose();
     _phoneNumberController.dispose();
-    _locationController.dispose();
+    // _locationController.dispose();
     super.dispose();
   }
 
@@ -35,19 +35,19 @@ class _AddOwnerPageState extends State<AddOwnerPage> {
     if (_formKey.currentState!.validate()) {
       String name = _nameController.text;
       String phoneNumber = _phoneNumberController.text;
-      String location = _locationController.text;
+      // String location = _locationController.text;
 
       // Save data to Firestore
-      FirebaseFirestore.instance.collection('owners').add({
+      FirebaseFirestore.instance.collection('drivers').add({
         'name': name,
         'phoneNumber': phoneNumber,
-        'location': location,
+        // 'location': location,
       });
 
       // Clear the form fields
       _nameController.clear();
       _phoneNumberController.clear();
-      _locationController.clear();
+      // _locationController.clear();
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const AdminPanelPage()),
@@ -59,7 +59,7 @@ class _AddOwnerPageState extends State<AddOwnerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Owner'),
+        title: const Text('Add Driver'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -88,16 +88,16 @@ class _AddOwnerPageState extends State<AddOwnerPage> {
                   return null;
                 },
               ),
-              TextFormField(
-                controller: _locationController,
-                decoration: const InputDecoration(labelText: 'Location'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a location';
-                  }
-                  return null;
-                },
-              ),
+              // TextFormField(
+              //   controller: _locationController,
+              //   decoration: const InputDecoration(labelText: 'Location'),
+              //   validator: (value) {
+              //     if (value == null || value.isEmpty) {
+              //       return 'Please enter a location';
+              //     }
+              //     return null;
+              //   },
+              // ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _submitForm,
--- lib/admin/drivers_page.dart
+++ lib/admin/drivers_page.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pixandrix/admin/add%20drivers/add_driver.dart';
 import 'package:pixandrix/theme/buttons/add_button.dart';
 
 class DriversPage extends StatelessWidget {
   const DriversPage({super.key});
-
-  void addDriver() {
-    // Add your logic to add a driver here
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0), // Add space on all sides
-        child: AddButton(text: 'Add Driver', onPressed: addDriver),
+        child: AddButton(text: 'Add Driver', onPressed: (){
+          Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const AddDriverPage()),
+      );
+        }),
       ),
     );
   }
