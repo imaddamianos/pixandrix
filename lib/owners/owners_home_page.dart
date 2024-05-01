2cfcc66e1bc980249b1c47698da16ef98f9b563b
iMad <damianos.imad@gmail.com>
2024-05-01 15:44:29 +0300

    adding order owner card

--- lib/owners/owners_home_page.dart
+++ lib/owners/owners_home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pixandrix/firebase/firebase_operations.dart';
 import 'package:pixandrix/first_page.dart';
 import 'package:pixandrix/helpers/order_form.dart';
+import 'package:pixandrix/models/order_model.dart';
 import 'package:pixandrix/models/owner_model.dart';
 import 'package:pixandrix/theme/buttons/main_button.dart';
+import 'package:pixandrix/widgets/order_card_owners.dart';
 
 class OwnersHomePage extends StatefulWidget {
   final OwnerData? ownerInfo;
@@ -14,11 +17,32 @@ class OwnersHomePage extends StatefulWidget {
 
 class _OwnersHomePageState extends State<OwnersHomePage> {
   late OwnerData? ownerInfo;
+  List<OrderData>? orders;
 
   @override
   void initState() {
     super.initState();
+    _loadOrders();
     ownerInfo = widget.ownerInfo;
+  }
+
+  Future<void> _removeOrder(int index) async {
+    // Remove the driver at the specified index from the list
+    if (index >= 0 && index < orders!.length) {
+      final orderToRemove = orders![index];
+      await FirebaseOperations.removeOrder(orderToRemove.orderID);
+      _loadOrders(); // Refresh the drivers list after removing the driver
+    }
+  }
+
+  Future<void> _loadOrders() async {
+    // Fetch drivers data and cast it to a List<DriverData>
+    final fetchedOrders = await FirebaseOperations.getOrders();
+    orders = fetchedOrders.cast<OrderData>();
+
+    if (mounted) {
+      setState(() {}); // Trigger a rebuild to reflect the updated drivers data
+    }
   }
 
   @override
@@ -27,9 +51,12 @@ class _OwnersHomePageState extends State<OwnersHomePage> {
       appBar: AppBar(
         title: Text(ownerInfo?.name ??
             'Owner Name'), // Use owner's name or a default value
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(ownerInfo?.ownerImage ??
-              ''), // Use owner's image or a default image URL
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(ownerInfo?.ownerImage ??
+                ''), // Use owner's image or a default image URL
+          ),
         ),
         actions: [
           Row(
@@ -52,17 +79,62 @@ class _OwnersHomePageState extends State<OwnersHomePage> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: CustomButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => OrderForm(ownerInfo: widget.ownerInfo),
-              ),
-            );
-          },
-          text: 'Request a Driver',
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        OrderForm(ownerInfo: widget.ownerInfo),
+                  ),
+                );
+              },
+              text: 'Request a Driver',
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: orders == null
+                  ? const Center(
+                      child:
+                          CircularProgressIndicator()) // Show loading indicator if drivers data is null
+                  : ListView.builder(
+                      itemCount: orders!.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            OrderCardOwners(
+                              orderTime: orders![index].orderTime,
+                              orderLocation: orders![index].orderLocation,
+                              status: orders![index].status,
+                              // isTaken: orders![index].isTaken,
+                              driverInfo: orders![index].driverInfo,
+                              storeInfo: orders![index].storeInfo,
+                              press: () {
+                                // showDialog(
+                                //   context: context,
+                                //   builder: (context) => DriverCardWindow(
+                                //     driverName: drivers![index].name,
+                                //     driverImage: drivers![index].driverImage,
+                                //     driverMobile: drivers![index].phoneNumber,
+                                //     driverID: drivers![index].driverID,
+                                //   ),
+                                // );
+                              },
+                              onCancel: () {
+                                _removeOrder(index);
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                          ],
+                        );
+                      },
+                    ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/order_card_owners.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pixandrix/helpers/order_status_utils.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class OrderCardOwners extends StatelessWidget {
+  const OrderCardOwners({
+    super.key,
+    required this.orderTime,
+    required this.orderLocation,
+    required this.status,
+    // required this.isTaken,
+    required this.driverInfo,
+    required this.storeInfo,
+    required this.press,
+    required this.onCancel,
+  });
+
+  final String driverInfo, orderLocation, storeInfo;
+  final String status;
+  // final bool isTaken;
+  final Timestamp orderTime; // Change the type to Timestamp
+  final GestureTapCallback press;
+  final VoidCallback onCancel;
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> statusInfo = getStatusInfo(status);
+    DateTime dateTime = orderTime.toDate(); // Convert Timestamp to DateTime
+    String timeAgo = timeago.format(dateTime, locale: 'en_short');
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - 20,
+          height: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        storeInfo,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        orderLocation,
+                        style: const TextStyle(color: Colors.green),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        '$timeAgo ago',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(height: 4),
+                      // Text(
+                      //   'Is Taken: $isTaken',
+                      //   style: const TextStyle(color: Colors.white),
+                      // ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromARGB(255, 184, 184, 183),
+                        Colors.transparent,
+                        Color.fromARGB(200, 184, 184, 183),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: IconButton(
+                    icon: Icon(
+                      statusInfo['iconData'],
+                      color: statusInfo['iconColor'],
+                    ),
+                    onPressed: (){},
+                  ),
+                ),
+                TextButton(
+              onPressed: onCancel,
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
+              ),
+            ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
