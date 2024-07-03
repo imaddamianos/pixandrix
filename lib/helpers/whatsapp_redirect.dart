c3d5b137be2f1800df63993065368c3db4fcc041
iMad <damianos.imad@gmail.com>
2024-07-04 02:49:20 +0300

    adding awsome notification library and fixing whatsapp

--- lib/helpers/whatsapp_redirect.dart
+++ lib/helpers/whatsapp_redirect.dart
@@ -1,16 +1,6 @@
-// whatsapp_redirect.dart
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> redirectToWhatsApp(String phoneNumber) async {
-  final Uri whatsappUri = Uri(
-    scheme: 'https',
-    host: 'wa.me',
-    path: phoneNumber,
-  );
-
-  if (await canLaunch(whatsappUri.toString())) {
-    await launch(whatsappUri.toString());
-  } else {
-    throw 'Could not launch $whatsappUri';
-  }
+  launchUrl(Uri.parse('https://wa.me/''$phoneNumber?text=Hi'),
+                    mode: LaunchMode.externalApplication);
 }
